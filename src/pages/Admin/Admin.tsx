0ea71af5eb244ee7086671dfeb70fc527338992e
neol10 <neol10@github.com>
2026-05-27 21:36:10 -0300

    fix: resolve admin photo crop setPhotoCropOnUrl missing import error

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -23,7 +23,7 @@ import { useDivisionContext } from '../../contexts/DivisionContext';
 import { isMissingColumnError as isMissingDivisionColumnError, markDivisionColumnMissing, markNightColumnMissing } from '../../lib/supabaseOptionalColumns';
 import { deriveMatchStatus } from '../../lib/matchStatus';
 import { rebuildStatsClientSide } from '../../lib/rebuildStats';
-import { clearPhotoCropFromUrl, parsePhotoCropFromUrl } from '../../lib/photoCrop';
+import { clearPhotoCropFromUrl, parsePhotoCropFromUrl, setPhotoCropOnUrl } from '../../lib/photoCrop';
 import {
   buildLocationFromCourt,
   COURT_OPTIONS,
